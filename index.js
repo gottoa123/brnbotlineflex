820e63e6dcd3f848769445c7c0a374a307a2efcc
gottoa123 <37652250+gottoa123@users.noreply.github.com>
2024-09-03 21:17:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -649,7 +649,7 @@ app.post("/webhook", async (req, res) => {
   res.sendStatus(200);
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
